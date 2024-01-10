e0b0a815f759a728f24d44a7e040e116330a1de6
Devtg7 <126765146+Devtg7@users.noreply.github.com>
2024-01-11 00:16:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-southeast-1"  # Adjust as needed
+}
+
+variable "create_s3" {
+  type = bool
+}
+
+resource "aws_s3_bucket_acl" "my_bucket" {
+  count = var.create_s3 ? 1 : 0
+  bucket = "my-bucket-${count.index + 1}"
+  acl    = "private"
+}
+
+resource "aws_instance" "my_instance" {
+  count = var.create_s3 ? 0 : 1
+  ami           = "ami-0c55b159cbfafe1f0"  # Change to a suitable AMI
+  instance_type = "t2.micro"
+}
